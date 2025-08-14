d264b406c6d56e6a8d4a8a241a0d61f97b8b9adb
thienngaa <pttnga10b2@gmail.com>
2025-08-14 14:43:21 +0700

    Create 03.js

---
+++ Session 10/Exercise 03/03.js
@@ -0,0 +1,4 @@
+let width = Number(prompt("Nhập chiều rộng của hình chữ nhật:"));
+let height = Number(prompt("Nhập chiều dài của hình chữ nhật:"));
+let area = width*height;
+document.write("Diện tích của hình chữ nhật là: " +　area);
