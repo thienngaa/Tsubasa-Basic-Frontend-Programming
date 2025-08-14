3ca2a36b19327b83552e594184c4916c46078ffe
thienngaa <pttnga10b2@gmail.com>
2025-08-14 15:00:18 +0700

    Create 06.js

---
+++ Session 10/Exercise 06/06.js
@@ -0,0 +1,5 @@
+let math = Number(prompt("Nhập điểm toán:"));
+let physics = Number(prompt("Nhập điểm lý:"));
+let chemistry = Number(prompt("Nhập điểm hóa:"));
+let average = (math+physics+chemistry)/3;
+alert("Điểm trung bình 3 môn học là: " + average);
