e2951ac59229e390d450fe9f3125cf2458e5bb72
thienngaa <pttnga10b2@gmail.com>
2025-09-17 22:10:55 +0700

    Create index.js

---
+++ Session 14/Exercise 2/index.js
@@ -0,0 +1,12 @@
+const car = {
+  brand: 'Toyota',
+  model: 'Corolla',
+  year: 2020,
+};
+
+// thêm thuộc tính
+car.color = 'red';
+// Cập nhậ giá trị
+car.year = 2022;
+// In ra đối tượng
+console.log(car);
