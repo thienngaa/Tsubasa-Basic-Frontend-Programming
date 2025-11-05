635614ef410a50992a81fc18205f3457db8b68a6
thienngaa <pttnga10b2@gmail.com>
2025-11-05 19:31:51 +0700

    Create Exercise4.sql

---
+++ Module 2. Database/Session 4/Exercise4.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Students (
+    StudentID INT PRIMARY KEY,
+    Name VARCHAR(100) NOT NULL,
+    Age INT,
+    Major VARCHAR(100)
+);
+INSERT INTO Students (StudentID, Name, Age, Major)
+VALUES
+(1, 'Alice', 20, 'Computer Science'),
+(2, 'Bob', 22, 'Mathematics'),
+(3, 'Charlie', 21, 'Physics');
+SELECT * FROM Students;
