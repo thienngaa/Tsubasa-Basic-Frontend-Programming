f08b411ef5e0ecb58b1924b6ccec1434c56e3c0d
thienngaa <pttnga10b2@gmail.com>
2025-11-24 13:29:37 +0700

    Create Exercise2.java

---
+++ Module 3. Java Fundamental/Session 01/Exercise2.java
@@ -0,0 +1,9 @@
+public class BaiTapTongHop {
+    public static void main(String[] args) {
+        // Hiển thị tên và tuổi ra màn hình
+        String fullName = "Nguyen Thi A";
+        int age = 21;
+        String address = "Nguyễn Trãi, Hà Nội";
+        System.out.printf("Tên tôi là %s, tôi %d tuổi, hiện tại đang học tại PTIT-HCM. Quê ở %s.",fullName,age,address);
+    }
+};
