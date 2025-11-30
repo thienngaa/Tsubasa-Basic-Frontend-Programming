0f7c7996b1415e01634c17d76bace7c99afa0440
thienngaa <pttnga10b2@gmail.com>
2025-11-30 12:38:24 +0700

    Create Age.java

---
+++ Module 3. Java Fundamental/Session 07/Exercise3/Age.java
@@ -0,0 +1,23 @@
+package exercise03;
+class Person {
+    private String name;
+    private int age;
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // Getter
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
