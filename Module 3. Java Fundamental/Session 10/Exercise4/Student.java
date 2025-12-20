08b5b29544f3dae1d02e0180f3b9bb230917d12a
thienngaa <pttnga10b2@gmail.com>
2025-12-20 21:35:38 +0700

    Create Student.java

---
+++ Module 3. Java Fundamental/Session 10/Exercise4/Student.java
@@ -0,0 +1,32 @@
+package exercise04;
+
+public class Student {
+    private int id;
+    private String name;
+
+    public Student(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "ID : " +id +" , Tên sinh viên: " + name;
+    }
+}
