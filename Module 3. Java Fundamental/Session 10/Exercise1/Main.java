396d0d2103bd2a6e441da65b156c422bfe95effd
thienngaa <pttnga10b2@gmail.com>
2025-12-20 21:30:26 +0700

    Create Main.java

---
+++ Module 3. Java Fundamental/Session 10/Exercise1/Main.java
@@ -0,0 +1,29 @@
+package exercise01;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        StudentManager manager = new StudentManager();
+        Scanner scanner = new Scanner(System.in);
+        String continueInput;
+
+        do {
+            String name = InputData.getString(scanner,"Nhập tên sinh viên: ");
+
+            int age = InputData.getInt(scanner ,"Nhập tuổi sinh viên: ");
+
+            double averageScore = InputData.getDouble(scanner , "Nhập điểm trung bình sinh viên: ");
+
+            Student student = new Student(name, age, averageScore);
+            manager.addStudent(student);
+
+
+            continueInput = InputData.getString(scanner,"Bạn có muốn thêm sinh viên khác? (y/n): ");
+        } while (continueInput.equalsIgnoreCase("y"));
+
+        System.out.println("\nDanh sách sinh viên:");
+        manager.displayStudents();
+        scanner.close();
+    }
+}
