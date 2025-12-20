ef8dca95395e8f9aab298822b8b210f43090461b
thienngaa <pttnga10b2@gmail.com>
2025-12-20 22:10:44 +0700

    Create Main.java

---
+++ Module 3. Java Fundamental/Session 14/Exercise4/Main.java
@@ -0,0 +1,9 @@
+package exercise04;
+
+public class Main {
+    public static void main(String[] args) {
+
+        BankManager bankManager = new BankManager();
+        bankManager.transfer(1,2,5000);
+    }
+}
