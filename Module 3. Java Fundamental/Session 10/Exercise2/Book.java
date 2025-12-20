d765f086205cc257dc514e6636ca4c945d21488e
thienngaa <pttnga10b2@gmail.com>
2025-12-20 21:31:53 +0700

    Create Book.java

---
+++ Module 3. Java Fundamental/Session 10/Exercise2/Book.java
@@ -0,0 +1,30 @@
+package exercise02;
+
+public class Book {
+    private String title;
+    private String author;
+    private int yearPublished;
+
+    public Book(String title, String author, int yearPublished) {
+        this.title = title;
+        this.author = author;
+        this.yearPublished = yearPublished;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    @Override
+    public String toString() {
+        return "Tên sách: " + title + ", Tác giả: " + author + ", Năm xuất bản: " + yearPublished;
+    }
+}
