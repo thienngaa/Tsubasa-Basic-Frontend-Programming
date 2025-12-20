7dff8677305af8b3d34f44f814b7b4267c1acf69
thienngaa <pttnga10b2@gmail.com>
2025-12-20 21:19:42 +0700

    Create Square.java

---
+++ Module 3. Java Fundamental/Session 08/Exercise3/Square.java
@@ -0,0 +1,23 @@
+package exercise03;
+
+public class Square implements Colorable {
+    private double side;
+    private String color;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getSide() {
+        return side;
+    }
+}
