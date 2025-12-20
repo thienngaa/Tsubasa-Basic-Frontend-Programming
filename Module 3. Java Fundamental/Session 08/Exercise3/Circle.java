e4b3abbe1e0adf0aa3f5e957c52298cf7de3e1a3
thienngaa <pttnga10b2@gmail.com>
2025-12-20 21:18:47 +0700

    Create Circle.java

---
+++ Module 3. Java Fundamental/Session 08/Exercise3/Circle.java
@@ -0,0 +1,23 @@
+package exercise03;
+
+public class Circle implements Colorable {
+    private double radius;
+    private String color;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+}
