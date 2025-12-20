da8db7d9c9fcec33e89e98f48e4746ce2e205919
thienngaa <pttnga10b2@gmail.com>
2025-12-20 22:07:36 +0700

    Create Student.java

---
+++ Module 3. Java Fundamental/Session 14/Exercise1/Student.java
@@ -0,0 +1,18 @@
+package exercise01;
+
+public class Student {
+    private int id;
+    private String name;
+    private int age;
+
+    public Student(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    // Getters and Setters
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public int getAge() { return age; }
+}
