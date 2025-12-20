d8135a49f129987b94233be7f36862f22f9f0b14
thienngaa <pttnga10b2@gmail.com>
2025-12-20 21:36:38 +0700

    Create Main.java

---
+++ Module 3. Java Fundamental/Session 11/Exercise1/Main.java
@@ -0,0 +1,23 @@
+package exercise01;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        // Tạo danh sách số nguyên
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        // Sử dụng Lambda Expression để in ra tất cả các số chẵn
+        System.out.println("Các số chẵn trong danh sách:");
+        numbers.forEach(number -> {
+            if (number % 2 == 0) {
+                System.out.println(number);
+            }
+        });
+
+        // Sử dụng phuương thức tham chiêu để tính tổng của tất cả các số trong danh sách
+        int sum = numbers.stream().mapToInt(Integer::intValue).sum();
+        System.out.println("Tổng của tất cả các số trong danh sách: " + sum);
+    }
+}
